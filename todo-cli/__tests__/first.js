e6866dbc2b9cc7209885d66e62d3f6f6b1e60140
arjun-88 <21951a0516@iare.ac.in>
2023-04-08 14:13:02 +0530

    todocli

--- todo-cli/__tests__/first.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-describe("First tset suite", () => {
-  test("First case", () => {
-    expect(true).toBe(true);
-  });
-});
--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -3,22 +3,68 @@
 
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
 
 describe("TodoList Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "testing todo",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+  });
+
   test("should add new todo", () => {
-    expect(all.length).toBe(0);
+    const todocount = all.length;
     add({
       title: "Test todo",
       completed: false,
-      dueDate: new Date().toISOString(),
+      dueDate: new Date().toISOString().split("T")[0],
     });
-    expect(all.length).toBe(1);
+    expect(all.length).toBe(todocount + 1);
   });
+
+  const Dateformating = (d) => {
+    return d.toISOString().split("T")[0];
+  };
+
+  const todaydate = new Date();
+
+  const yesterday = Dateformating(
+    new Date(new Date().setDate(todaydate.getDate() - 1))
+  );
+
+  const tomorrow = Dateformating(
+    new Date(new Date().setDate(todaydate.getDate() + 1))
+  );
 
   test("Should mark a todo as complete", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+
+  test("Should get overdue items", () => {
+    const dueitems = {
+      title: "Test overdue",
+      completed: false,
+      dueDate: yesterday,
+    };
+    add(dueitems);
+    expect(overdue().length).toBe(1);
+  });
+
+  test("should get today due items", () => {
+    expect(dueToday().length).toBe(2);
+  });
+
+  test("Should retrieve Later due items", () => {
+    const dueitems = {
+      title: "Test dueLater",
+      completed: false,
+      dueDate: tomorrow,
+    };
+    add(dueitems);
+    expect(dueLater().length).toBe(1);
   });
 });
